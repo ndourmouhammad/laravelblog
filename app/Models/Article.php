d863eb63e69021fd9b321f175a57a7c0cf1e99ac
ndourmouhammad <ndourmouhammad@gmail.com>
2024-05-28 16:56:34 +0000

    definir la relation dans le model article

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -7,5 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    use HasFactory;
+    protected $fillable = ['nom', 'description', 'image_url', 'date_creation', 'a_la_une'];
+
+    public function commentaires()
+    {
+        return $this->hasMany(Commentaire::class);
+    }
 }
+
