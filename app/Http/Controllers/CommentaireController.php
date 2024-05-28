5996a12efdf2fa166ab2fce54ba55fd54578afa0
ndourmouhammad <ndourmouhammad@gmail.com>
2024-05-28 18:06:43 +0000

    Ajoutez les méthodes nécessaires pour le CRUD des commentaires.

--- app/Http/Controllers/CommentaireController.php
+++ app/Http/Controllers/CommentaireController.php
@@ -3,8 +3,55 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Commentaire;
+use App\Models\Article;
 
 class CommentaireController extends Controller
 {
-    //
+    public function create($articleId)
+    {
+        $article = Article::findOrFail($articleId);
+        return view('commentaires.create', compact('article'));
+    }
+
+    public function store(Request $request, $articleId)
+    {
+        $validated = $request->validate([
+            'contenu' => 'required',
+            'nom_complet_auteur' => 'required',
+        ]);
+
+        $validated['article_id'] = $articleId;
+        Commentaire::create($validated);
+
+        return redirect()->route('articles.show', $articleId);
+    }
+
+    public function edit($id)
+    {
+        $commentaire = Commentaire::findOrFail($id);
+        return view('commentaires.edit', compact('commentaire'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validated = $request->validate([
+            'contenu' => 'required',
+            'nom_complet_auteur' => 'required',
+        ]);
+
+        $commentaire = Commentaire::findOrFail($id);
+        $commentaire->update($validated);
+
+        return redirect()->route('articles.show', $commentaire->article_id);
+    }
+
+    public function destroy($id)
+    {
+        $commentaire = Commentaire::findOrFail($id);
+        $commentaire->delete();
+
+        return redirect()->route('articles.show', $commentaire->article_id);
+    }
 }
+
